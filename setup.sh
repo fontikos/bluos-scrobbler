c8cb016769a161e0b14b9499bf9371a163442ad7
Xenofon (Fontas) Fafoutis <xefa@dtu.dk>
2022-04-18 20:41:53 +0200

    minor

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # Set up as a system service
 sudo cp bluos-scrobbler.service /etc/systemd/system/
 sudo systemctl daemon-reload
-systemctl enable bluos-scrobbler.service
+sudo systemctl enable bluos-scrobbler.service
 sudo systemctl restart bluos-scrobbler.service
 
 # To follow the output:
