d6adcfb38519dad3d1134a46444a980fbca197fe
Xenofon (Fontas) Fafoutis <xefa@dtu.dk>
2022-04-18 20:51:06 +0200

    test

--- setup.sh
+++ setup.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 # Set up as a system service
+
+PWD=`pwd`
+ME=`whoami`
+FILE="[Unit]
+Description=BluOS Scrobbler
+After=multi-user.target
+
+[Service]
+Type=simple
+Restart=always
+Environment=PYTHONUNBUFFERED=1
+User=$ME
+ExecStart=/usr/bin/python3 $PWD/bluos-scrobbler.py
+StandardOutput=journal+console
+StandardError=journal+console
+
+[Install]
+WantedBy=multi-user.target
+"
+
+echo $FILE > bluos-scrobbler.service
+exit 0
+
 sudo cp bluos-scrobbler.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable bluos-scrobbler.service
